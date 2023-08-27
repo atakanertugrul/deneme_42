198e2216345ab9cd8a337a562cd09ba1e989ba16
atakanertugrul <atakanertugrul61@gmail.com>
2023-08-27 12:46:10 +0300

    ilk deneme

---
+++ Playground.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+int islem=0;
+
+int a=0;
+int b=0;
+int result=0;
+
+printf("Yapılacak işlemi seç: 1- toplama, 2-çıkarma\n");
+scanf("%d", &islem);
+
+printf("İlk sayıyı giriniz:\n");
+scanf("%d", &a);
+
+printf("İkinci sayıyı giriniz:\n");
+scanf("%d", &b);
+
+if(islem==1){
+result = a+b;
+}else{
+result = a-b;}
+
+printf("Sonuç: %d\n", result);
+
+return 0;
+
+}
+
+
+
+
+
