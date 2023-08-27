59cba294a696c33d065e18cd17932f51d3f5a4f1
atakanertugrul <atakanertugrul61@gmail.com>
2023-08-27 12:58:04 +0300

    üçüncü deneme

---
+++ BitwiseOperators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	unsigned int a = 60; // 0011 1100
+	unsigned int b = 13; // 0000 1101
+
+	int result;
+	int shifted;
+
+	result = a & b; // 0000 1100
+
+	shifted = a << 2;
+
+	printf("Result: %d\n", result);
+	printf("~Result: %d\n", ~result);
+	printf("Shifted a: %d\n", shifted);
+	printf("Initial a: %u\nInitial b: %u\n", a, b);
+
+	return 0;
+}
